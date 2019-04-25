c34206eeb1f5efe40c8ae13b4d006bb737c5b48e
senlin.yang <senlin.yang@hand-china.com>
2019-04-25 21:27:00 +0800

    添加Lombok

--- src/main/java/com/core/demo/dto/UserDTO.java
+++ src/main/java/com/core/demo/dto/UserDTO.java
@@ -1,10 +1,18 @@
 package com.core.demo.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * @author senlin.yang@hand-china.com
  * @version V1.0
  * @Date 2019-4-4
  * @description
  */
+
+@Getter
+@Setter
+@ToString
 public class UserDTO {
 }
